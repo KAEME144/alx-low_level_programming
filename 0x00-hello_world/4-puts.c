39cd50c9c0cca93a8b6a8b1164f25b0b7eacf1a5
KAEME144 <kaeme144@gmail.com>
2023-09-12 14:58:28 -0700

    Hello c puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+* main - Write a program that prints the given text with puts funtion.
+* Return:0
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
